409853f08b2225b4fe0432e152d686a46dff3b73
SivaprabaSivakumar <sivaprabz27@gmail.com>
2026-03-26 08:44:40 +0530

    Praba | playwright config file clean up

--- playwright.config.ts
+++ playwright.config.ts
@@ -13,26 +13,20 @@ import { defineConfig, devices } from '@playwright/test';
  */
 export default defineConfig({
   testDir: './tests',
-  /* Run tests in files in parallel */
   fullyParallel: false,
-  /* Fail the build on CI if you accidentally left test.only in the source code. */
-  forbidOnly: !!process.env.CI,
-  /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
-  /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 1 : 1,
-  /* Reporter to use. See https://playwright.dev/docs/test-reporters */
+  workers: 1,
+  // workers: process.env.CI ? 1 : 1,
   reporter: [['html'], ['list']],
+  use: {},
   // use: {
   //   extraHTTPHeaders: {
-
   //   },
   //   httpCredentials: {
   //     username: '',
   //     password: ''
   //   }
   // },
-  use: {}
 
   /* Configure projects for major browsers */
   projects: [
